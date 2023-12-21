b5cb252a84e553d9cf0e17a80a252f81b9959612
George Grainger <georgegrainger1008@gmail.com>
2023-12-21 14:40:19 +0000

    Finish day 19 part 2

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -46,7 +46,7 @@ impl TryFrom<&str> for Operator {
 struct Rule<'a> {
     key: &'a str,
     operator: Operator,
-    condition: usize,
+    condition: u64,
     target: &'a str,
 }
 
@@ -59,7 +59,7 @@ struct RuleEntry<'a> {
 pub struct Day19 {}
 
 impl Day19 {
-    fn deserialize_parts(parts: &str) -> Vec<HashMap<&str, usize>> {
+    fn deserialize_parts(parts: &str) -> Vec<HashMap<&str, u64>> {
         parts
             .lines()
             .map(|dict| {
@@ -72,7 +72,7 @@ impl Day19 {
                     .split(',')
                     .map(|entry| {
                         let (k, v) = entry.split_once('=').expect("Entry should be 'k=v'");
-                        let v = v.parse::<usize>().unwrap();
+                        let v = v.parse::<u64>().unwrap();
                         (k, v)
                     })
                     .collect()
@@ -107,8 +107,8 @@ impl Day19 {
                             .split_once(Operator::is_valid)
                             .expect("Should contain '>' or '<'");
 
-                        // convert the condition to a usize and create the rule
-                        let condition = condition.parse::<usize>().unwrap();
+                        // convert the condition to a u64 and create the rule
+                        let condition = condition.parse::<u64>().unwrap();
                         Rule {
                             key,
                             operator,
@@ -136,12 +136,12 @@ impl Day for Day19 {
         let mut total = 0;
         for part in parts {
             // Default rule key
-            let mut key = "in";
-            while key != "A" && key != "R" {
+            let mut rule_key = "in";
+            while rule_key != "A" && rule_key != "R" {
                 let rule_entry = ruleset
-                    .get(&key)
-                    .expect(format!("Key {key} should be in rule set").as_str());
-                key = rule_entry
+                    .get(&rule_key)
+                    .expect(format!("Key {rule_key} should be in rule set").as_str());
+                rule_key = rule_entry
                     .rules
                     .iter()
                     .find_map(
@@ -161,8 +161,8 @@ impl Day for Day19 {
                     .unwrap_or(&rule_entry.default);
             }
 
-            if key == "A" {
-                total += part.values().sum::<usize>();
+            if rule_key == "A" {
+                total += part.values().sum::<u64>();
             }
         }
 
@@ -170,6 +170,83 @@ impl Day for Day19 {
     }
 
     fn problem2() {
-        todo!();
+        let (rules, _) = SRC
+            .split_once("\r\n\r\n")
+            .expect("File should contain list of rules, then line seperator, then list of parts");
+
+        let ruleset = Self::deserialize_ruleset(rules);
+
+        // Create initial state
+        let mut total = 0;
+        let mut state = HashMap::with_capacity(4);
+        state.insert("x", (1, 4000));
+        state.insert("a", (1, 4000));
+        state.insert("m", (1, 4000));
+        state.insert("s", (1, 4000));
+
+        // Create stack of all states
+        let mut stack = Vec::new();
+        stack.push(("in", state));
+
+        while let Some((rule_key, mut state)) = stack.pop() {
+            if rule_key == "R" {
+                continue;
+            }
+
+            if rule_key == "A" {
+                total += state
+                    .values()
+                    .map(|(min, max)| (max + 1) - min)
+                    .product::<u64>();
+                continue;
+            }
+
+            let rule_entry = ruleset
+                .get(&rule_key)
+                .expect(format!("Key {rule_key} should be in rule set").as_str());
+
+            for Rule {
+                key,
+                operator,
+                condition,
+                target,
+            } in rule_entry.rules.iter()
+            {
+                let (curr_min, curr_max) = state.get(key).unwrap();
+                match operator {
+                    Less => {
+                        if condition < curr_min {
+                            continue;
+                        }
+                        assert!(condition < curr_max);
+
+                        // Add value taking path
+                        let mut new_state = state.clone();
+                        new_state.insert(key, (*curr_min, *condition - 1));
+                        stack.push((target, new_state));
+
+                        // Update state for not taking path
+                        state.insert(key, (*condition, *curr_max));
+                    }
+                    Greater => {
+                        if condition > curr_max {
+                            continue;
+                        }
+                        assert!(condition > curr_min);
+
+                        // Add value taking path
+                        let mut new_state = state.clone();
+                        new_state.insert(key, (*condition + 1, *curr_max));
+                        stack.push((target, new_state));
+
+                        // Update state for not taking path
+                        state.insert(key, (*curr_min, *condition));
+                    }
+                }
+            }
+            stack.push((rule_entry.default, state));
+        }
+
+        println!("{}", total);
     }
 }
