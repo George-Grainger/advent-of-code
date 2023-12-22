70ff97f50b782c9102957e658e3505c754fab53f
George Grainger <georgegrainger1008@gmail.com>
2023-12-22 12:43:35 +0000

    Effectively reimplemented same idea but more efficiently

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,25 +1,31 @@
 use advent_of_code::Day;
-use std::collections::{BTreeSet, HashSet};
+use std::collections::HashSet;
 
 const SRC: &str = include_str!("../../input/day21.txt");
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct State {
-    x: usize,
-    y: usize,
-    steps: usize,
-}
-
-impl State {
-    fn new(x: usize, y: usize, steps: usize) -> Self {
-        Self { x, y, steps }
-    }
-}
+type Coord = (usize, usize);
 
 pub struct Day21 {}
 
 impl Day21 {
-    fn get_neighbours(x: usize, y: usize, max_x: usize, max_y: usize) -> Vec<(usize, usize)> {
+    fn parse_input() -> (Vec<Vec<char>>, Coord, Coord) {
+        let map: Vec<Vec<char>> = SRC.lines().map(|s| s.chars().collect()).collect();
+        let height = map.len();
+        let width = map.get(0).expect("Should be at least one row").len();
+        let lim = (width - 1, height - 1);
+
+        // Find the 'S' position
+        let start = map
+            .iter()
+            .flatten()
+            .position(|&c| c == 'S')
+            .expect("Should be 'S' in map");
+        let start = (start / height, start % height);
+
+        (map, start, lim)
+    }
+
+    fn get_neighbours((x, y): Coord, (max_x, max_y): Coord) -> Vec<Coord> {
         let mut neighbours: Vec<_> = Vec::with_capacity(4);
         if y > 0 {
             neighbours.push((x, y - 1))
@@ -35,57 +41,35 @@ impl Day21 {
         }
         neighbours
     }
-
-    fn print(map: &Vec<Vec<char>>) {
-        for line in map.iter() {
-            for &c in line.iter() {
-                print!("{:} ", c as char);
-            }
-            println!();
-        }
-    }
 }
 
 impl Day for Day21 {
     fn problem1() {
-        let map: Vec<Vec<char>> = SRC.lines().map(|s| s.chars().collect()).collect();
-        let height = map.len();
-        let width = map.get(0).expect("Should be at least one row").len();
-        let max_x = width - 1;
-        let max_y = height - 1;
-
-        // Find the 'S' position
-        let start = map
-            .iter()
-            .flatten()
-            .position(|&c| c == 'S')
-            .expect("Should be 'S' in map");
-        let start = State::new(start % height, start / height, 0);
+        const MAX_STEPS: usize = 64;
+        let (map, start, lim) = Self::parse_input();
 
         // Create a set for holding active positions
-        let mut active_positions: BTreeSet<State> = BTreeSet::new();
+        let mut active_positions: HashSet<Coord> = HashSet::new();
         active_positions.insert(start);
 
-        // Create list of final positions
-        const MAX_STEPS: usize = 12;
-        let mut final_positions = HashSet::new();
+        // Iterate over each step to find active positions
+        for _ in 0..MAX_STEPS {
+            let mut new_active_positions = HashSet::new();
 
-        while let Some(pos) = active_positions.pop_first() {
-            if pos.steps == MAX_STEPS {
-                final_positions.insert(pos);
-            } else {
-                // Get neighbours that aren't rocks and insert into the active_positions
-                Self::get_neighbours(pos.x, pos.y, max_x, max_y)
+            for position in active_positions {
+                let next_poisitions = Self::get_neighbours(position, lim)
                     .into_iter()
-                    .filter(|&(x, y)| map[y][x] != '#')
-                    .map(|(x, y)| State::new(x, y, pos.steps + 1))
-                    .for_each(|neighbour| {
-                        active_positions.insert(neighbour);
-                    });
+                    .filter(|&(x, y)| map[y][x] != '#');
+
+                for np in next_poisitions {
+                    new_active_positions.insert(np);
+                }
             }
+
+            active_positions = new_active_positions;
         }
 
-        println!("{:?}", final_positions.len());
+        println!("{:?}", active_positions.len());
     }
 
     fn problem2() {}
